47740aa2d6364f6d81925e0990dfdd1c4881ced8
Sajal Goyal <sajalg@iitk.ac.in>
2020-05-27 15:34:11 +0530

    Update atari.py

--- examples/DeepQNetwork/atari.py
+++ examples/DeepQNetwork/atari.py
@@ -118,7 +118,9 @@ class AtariPlayer(gym.Env):
             if isinstance(self.viz, float):
                 #cv2.imshow(self.windowname, ret)
                 cv2_imshow(ret)
-                cv2.waitKey(int(self.viz * 1000))
+                #cv2.waitKey(int(self.viz * 1000))
+#                 plt.imshow(ret)
+#                 plt.show()
         ret = ret.astype('float32')
         # 0.299,0.587.0.114. same as rgb2y in torch/image
         ret = cv2.cvtColor(ret, cv2.COLOR_RGB2GRAY)[:, :]
